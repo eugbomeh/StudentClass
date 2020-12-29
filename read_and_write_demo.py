399b864e8d717c8956b0227d7c2d9e1cf34cd7cb
Eli <correcteli@gmail.com>
2020-12-28 22:52:38 -0600

    changes to read and write file

--- read_and_write_demo.py
+++ read_and_write_demo.py
@@ -1,10 +1,3 @@
-## Reading from and writing to files
-# We learned the basics of using context managers to read data
-# from a file, make sure to have the data.txt file in the same
-# directory as this file, otherwise you have to provide the full
-# path to the file
-
-
 file_name = 'data.txt'
 with open(file_name) as f:
     for line in f:
