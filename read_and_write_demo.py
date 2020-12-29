9d5750d49c57e1f9ad5304bf4bf34e711685ae91
Eli <correcteli@gmail.com>
2020-12-28 23:08:02 -0600

    Changes

--- read_and_write_demo.py
+++ read_and_write_demo.py
@@ -4,5 +4,14 @@ with open(file_name) as f:
         print(line.strip())
 
 
+record_to_add = "john,schmoe:python,ruby,javascript"
+
+
+
+with open(file_name, "a+") as to_write:
+    to_write.write("\n"+record_to_add)
+
+
+
 
 
